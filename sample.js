211585d50e41f691ddb046104540dfb9d8e0d87d
ss4223 <>
2019-03-21 17:34:31 +0900

    4th

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 (window.onload = function() {
 	document.querySelector('#aaa').innerHTML = "VVVVVVvv";
-	fetch('fp1.bn').then(function(response) {
+	fetch('fp1.bin').then(function(response) {
 		return response.blob();
 	  }).then(function(blob) {
 		// blobにBlob型で結果が渡される
