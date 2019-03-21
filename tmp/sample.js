c39bc37b8671e6977c9960ea46e0fb0769f7e5cc
ss4223 <>
2019-03-21 17:42:14 +0900

    next

--- tmp/sample.js
+++ tmp/sample.js
@@ -1,6 +1,6 @@
 (window.onload = function() {
 	document.querySelector('#aaa').innerHTML = "VVVVVVvv";
-	fetch('https://github.com/ika-gyosen/test_mydfu/blob/master/NUCLEO_F446RE.bin').then(function(response) {
+	fetch('fp1.bin').then(function(response) {
 		return response.blob();
 	  }).then(function(blob) {
 		// blobにBlob型で結果が渡される
