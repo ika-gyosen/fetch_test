f2925e691b42530243a883cd3b08630ebc0fdb9d
ss4223 <>
2019-03-21 17:48:01 +0900

    url

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 (window.onload = function() {
 	document.querySelector('#aaa').innerHTML = "VVVVVVvv";
-	fetch('fp1.bin').then(function(response) {
+	fetch('https://cdn.shopify.com/s/files/1/2681/3762/files/ParipiDestroyer_v1-1b.NUCLEO_L433RC_P-17.bin?1').then(function(response) {
 		return response.blob();
 	  }).then(function(blob) {
 		// blobにBlob型で結果が渡される
